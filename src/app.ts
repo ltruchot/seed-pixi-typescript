19f7e28359dae29c8958b9447dc4fb702c4b5d10
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2021-05-24 10:05:45 +0200

    feat(main): add FPS

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import {
-  Application, Loader, Resource, Texture,
+  Application, Loader, Resource, Text, Texture, Ticker,
 } from 'pixi.js';
 import {
   createEntity, getNextEntityDirection, getNextEntityPosition,
@@ -14,22 +14,30 @@ const app = new Application({
   width: SIZE,
   height: SIZE,
   backgroundColor: 0x1099bb, // light blue
+  sharedTicker: true,
+  sharedLoader: true,
 });
 document.body.appendChild(app.view);
+const loader = Loader.shared;
+const ticker = Ticker.shared;
 
 // preload needed assets
-const loader = Loader.shared;
 loader.add('samir', '/assets/img/hero.png');
 
 // when loader is ready
 loader.load(() => {
+  // create and append FPS text
+  const fps = new Text('FPS: 0', { fill: 0xffffff });
+  app.stage.addChild(fps);
+
   // create and append hero
   const heroTexture = loader.resources.samir.texture as Texture<Resource>;
   const hero = createEntity(heroTexture, CENTER, CENTER);
   app.stage.addChild(hero.sprite);
 
   // animate hero each "tick": go left or right continuously
-  app.ticker.add(() => {
+  ticker.add(() => {
+    fps.text = `FPS: ${ticker.FPS.toFixed(2)}`;
     hero.direction = getNextEntityDirection(app.view.width, hero);
     hero.sprite.x = getNextEntityPosition(hero);
   });
